2eb2c5a8e0664b9c8b63b7a6c66660b34c1322cd
Oliver Lu <pa596bu500@gmail.com>
2023-03-23 06:40:05 +0000

    tried test

--- test/xt_ui_test.dart
+++ test/xt_ui_test.dart
@@ -1,6 +1,8 @@
 import 'package:flutter_test/flutter_test.dart';
-
+import 'package:flutter/material.dart';
 import 'package:xt_ui/xt_ui.dart';
+// import '../lib/wdgt/xtInfoBox.dart';
+// import '../lib/xt_helpers.dart';
 
 void main() {
   // test('adds one to input values', () {
@@ -9,4 +11,22 @@ void main() {
   //   expect(calculator.addOne(-7), -6);
   //   expect(calculator.addOne(0), 1);
   // });
+  testWidgets('MyWidget has a title and message', (tester) async {
+    // Test code goes here.
+    await tester.pumpWidget(
+      const MaterialApp(
+        home: xtInfoBox(
+          width: 300,
+          boarderColor: xtLightGreen2,
+          borderRadius: 15,
+          text: 'Meter Reading',
+          textColor: Colors.white,
+          icon: Icon(
+            Icons.check_circle,
+            color: xtLightGreen2,
+          ),
+        ),
+      ),
+    );
+  });
 }
