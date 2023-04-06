774df90e758d02690510bc89ce57ed51ab703655
Oliver Lu <pa596bu500@gmail.com>
2023-04-06 10:03:14 +0000

    add xtWait size

--- lib/wdgt/xtWait.dart
+++ lib/wdgt/xtWait.dart
@@ -3,16 +3,17 @@ import 'package:loading_animation_widget/loading_animation_widget.dart';
 import 'package:xt_ui/xt_ui.dart';
 
 class xtWait extends StatelessWidget {
-  xtWait({Key? key, this.color}) : super(key: key);
+  xtWait({Key? key, this.color, this.size}) : super(key: key);
 
   Color? color;
+  double? size;
 
   @override
   Widget build(BuildContext context) {
     return Container(
       child: LoadingAnimationWidget.staggeredDotsWave(
         color: color ?? xtLightGreen1,
-        size: 21,
+        size: size ?? 21,
       ),
     );
   }
