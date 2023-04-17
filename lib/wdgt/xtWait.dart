56e48d385b0c48108216cef84b862836288d90e6
Oliver Lu <pa596bu500@gmail.com>
2023-04-17 04:13:48 +0000

    update xtWait for more anim

--- lib/wdgt/xtWait.dart
+++ lib/wdgt/xtWait.dart
@@ -3,18 +3,32 @@ import 'package:loading_animation_widget/loading_animation_widget.dart';
 import 'package:xt_ui/xt_ui.dart';
 
 class xtWait extends StatelessWidget {
-  xtWait({Key? key, this.color, this.size}) : super(key: key);
+  xtWait({Key? key, this.color, this.size, this.anim}) : super(key: key);
 
   Color? color;
   double? size;
+  String? anim;
 
   @override
   Widget build(BuildContext context) {
+    Widget wait;
+    switch (anim) {
+      case 'horizontalRotatingDots':
+        wait = LoadingAnimationWidget.horizontalRotatingDots(
+          color: color ?? xtLightGreen1,
+          size: size ?? 21,
+        );
+        break;
+      default:
+        wait = LoadingAnimationWidget.staggeredDotsWave(
+          color: color ?? xtLightGreen1,
+          size: size ?? 21,
+        );
+        break;
+    }
+
     return Container(
-      child: LoadingAnimationWidget.staggeredDotsWave(
-        color: color ?? xtLightGreen1,
-        size: size ?? 21,
-      ),
+      child: wait,
     );
   }
 }
