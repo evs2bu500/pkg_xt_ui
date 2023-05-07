5d75b3a1b054853391bc25086a0545056685d90c
ggbotbox <oliver.lu@pa.com.sg>
2023-05-07 22:59:08 +0800

    enhance xtInfoBox

--- lib/wdgt/xtInfoBox.dart
+++ lib/wdgt/xtInfoBox.dart
@@ -3,22 +3,26 @@ import 'package:flutter/material.dart';
 import 'package:xt_ui/xt_ui.dart';
 
 class xtInfoBox extends StatelessWidget {
-  const xtInfoBox(
-      {Key? key,
-      this.width,
-      this.height,
-      this.icon,
-      this.text,
-      this.textColor,
-      this.boarderColor,
-      this.borderRadius,
-      // this.fontSize,
-      // this.fontstyle,
-      this.textStyle,
-      this.padding})
-      : super(key: key);
+  const xtInfoBox({
+    Key? key,
+    this.width,
+    this.height,
+    this.icon,
+    this.iconTextPadding,
+    this.text,
+    this.textColor,
+    this.boarderColor,
+    this.borderRadius,
+    // this.fontSize,
+    // this.fontstyle,
+    this.textStyle,
+    this.padding,
+    this.superText,
+    this.superTextStyle,
+  }) : super(key: key);
 
   final Icon? icon;
+  final double? iconTextPadding;
   final String? text;
   final Color? boarderColor;
   final Color? textColor;
@@ -29,6 +33,8 @@ class xtInfoBox extends StatelessWidget {
   // final double? fontSize;
   // final FontStyle? fontstyle;
   final EdgeInsetsGeometry? padding;
+  final String? superText;
+  final TextStyle? superTextStyle;
 
   @override
   Widget build(BuildContext context) {
@@ -37,30 +43,41 @@ class xtInfoBox extends StatelessWidget {
         border: Border.all(color: boarderColor ?? xtTransParent),
         borderRadius: BorderRadius.circular(borderRadius ?? 0),
       ),
-      padding: padding ?? const EdgeInsets.all(13.0),
+      padding: padding ?? const EdgeInsets.all(8.0),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.center,
         mainAxisSize: MainAxisSize.min,
-        crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(right: 8),
-                child: icon,
-              ),
-              Center(
-                child: Text(text ?? '',
-                    style: textStyle ??
-                        TextStyle(
-                            color: textColor ?? Colors.white,
-                            fontSize: 17,
-                            fontStyle: FontStyle.normal)),
-              ),
-            ],
+          Padding(
+            padding: const EdgeInsets.only(right: 3),
+            child: icon,
           ),
+          SizedBox(width: iconTextPadding ?? 0),
+          // Text(text ?? '',
+          //     style: textStyle ??
+          //         TextStyle(
+          //             color: textColor ?? Colors.white,
+          //             fontSize: 17,
+          //             fontStyle: FontStyle.normal)),
+          RichText(
+            text: TextSpan(
+              children: [
+                TextSpan(text: text ?? '', style: textStyle),
+                superText != null
+                    ? WidgetSpan(
+                        child: Transform.translate(
+                          offset: const Offset(2.0, -13.0),
+                          child: Text(
+                            superText!,
+                            style:
+                                superTextStyle ?? const TextStyle(fontSize: 10),
+                          ),
+                        ),
+                      )
+                    : const TextSpan(),
+              ],
+            ),
+          )
         ],
       ),
     );
