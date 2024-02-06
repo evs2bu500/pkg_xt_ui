fdbfc3122c47b92e3bd793d6ea46f8b98d647731
ggbotbox <oliver.lu@pa.com.sg>
2024-02-06 23:20:53 +0800

    add popup button

---
+++ lib/wdgt/wgt_popup_button.dart
@@ -0,0 +1,139 @@
+import 'package:flutter/material.dart';
+
+class WgtPopupButton extends StatelessWidget {
+  const WgtPopupButton({
+    Key? key,
+    // this.buttonKey,
+    required this.child,
+    required this.width,
+    required this.height,
+    required this.popupWidth,
+    required this.popupHeight,
+    required this.popupChild,
+    this.backgroundColor,
+    this.direction,
+    this.onHover,
+    this.onTap,
+    this.disabled,
+    this.xOffset = 0,
+  }) : super(key: key);
+
+  // final GlobalKey buttonKey = GlobalKey();
+  final Widget child;
+  final double width;
+  final double height;
+  final double popupWidth;
+  final double popupHeight;
+  final Widget popupChild;
+  final Color? backgroundColor;
+  final String? direction;
+  final Function()? onTap;
+  final Function(bool val)? onHover;
+  final bool? disabled;
+  final double xOffset;
+
+  @override
+  Widget build(BuildContext context) {
+    GlobalKey buttonKey = GlobalKey();
+
+    return InkWell(
+      onTap: (disabled != null && disabled!)
+          ? null
+          : () {
+              if (onTap != null) {
+                onTap!();
+              }
+              final renderBox =
+                  buttonKey.currentContext?.findRenderObject() as RenderBox;
+              final position = renderBox.localToGlobal(Offset.zero);
+
+              bool toCloseToBottom = false;
+              //check if the popup is too close to the bottom of the screen
+              if (position.dy + popupHeight >
+                  MediaQuery.of(context).size.height) {
+                toCloseToBottom = true;
+              }
+              double vertialOffset = 0;
+              if (toCloseToBottom) {
+                vertialOffset = popupHeight;
+              }
+
+              showDialog(
+                context: context,
+                builder: (context) {
+                  //offset between center of the screen and the button
+                  late Offset offset;
+
+                  if (direction == null || direction == 'right') {
+                    offset = Offset(
+                        position.dx -
+                            MediaQuery.of(context).size.width / 2 +
+                            popupWidth / 2 +
+                            width / 2 +
+                            xOffset,
+                        position.dy -
+                            MediaQuery.of(context).size.height / 2 +
+                            popupHeight / 2 +
+                            height / 2 -
+                            vertialOffset);
+                  } else {
+                    offset = Offset(
+                        position.dx -
+                            MediaQuery.of(context).size.width / 2 -
+                            popupWidth / 2 +
+                            xOffset,
+                        // -width / 2,
+                        position.dy -
+                            MediaQuery.of(context).size.height / 2 +
+                            popupHeight / 2 +
+                            height / 2 -
+                            vertialOffset);
+                  }
+
+                  return Transform.translate(
+                    offset: offset,
+                    child: Dialog(
+                      elevation: 0,
+                      backgroundColor: Colors.transparent,
+                      child: Stack(
+                        alignment: AlignmentDirectional.center,
+                        children: [
+                          Container(
+                            width: popupWidth,
+                            height: popupHeight,
+                            decoration: BoxDecoration(
+                              color: backgroundColor ??
+                                  Theme.of(context)
+                                      .scaffoldBackgroundColor
+                                      .withOpacity(0.8),
+                              borderRadius: BorderRadius.circular(5),
+                              boxShadow: [
+                                BoxShadow(
+                                  color: Theme.of(context)
+                                      .hintColor
+                                      .withOpacity(0.1),
+                                  spreadRadius: 0,
+                                  blurRadius: 2,
+                                  offset: const Offset(0, 2),
+                                ),
+                              ],
+                            ),
+                            child: popupChild,
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+                },
+              );
+            },
+      onHover: onHover,
+      child: SizedBox(
+        width: width,
+        height: height,
+        key: buttonKey,
+        child: child,
+      ),
+    );
+  }
+}
--- lib/wdgt/xt_key_value_text.dart
+++ lib/wdgt/xt_key_value_text.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:xt_util/xt_util.dart';
 
-class xtKeyValueText extends StatelessWidget {
+import 'wgt_popup_button.dart';
+
+class xtKeyValueText extends StatefulWidget {
   //boilerplate
   const xtKeyValueText({
     Key? key,
@@ -12,7 +16,8 @@ class xtKeyValueText extends StatelessWidget {
     this.valueWidth,
     this.valueLoadingAnim,
     this.spaceInBetween = 5,
-    this.selectable,
+    this.selectable = true,
+    this.clickCopy = false,
   }) : super(key: key);
 
   final String keyText;
@@ -23,48 +28,52 @@ class xtKeyValueText extends StatelessWidget {
   final double? valueWidth;
   final double spaceInBetween;
   final Widget? valueLoadingAnim;
-  final bool? selectable;
+  final bool selectable;
+  final bool clickCopy;
+
+  @override
+  State<xtKeyValueText> createState() => _xtKeyValueTextState();
+}
+
+class _xtKeyValueTextState extends State<xtKeyValueText> {
+  String _currentFullText = '';
+  @override
+  void initState() {
+    super.initState();
+
+    _currentFullText = widget.valueText;
+    // _controller.text = lengthyString(
+    //     widget.valueText,
+    //     getStringDisplaySize(
+    //             widget.originalFullText,
+    //             TextStyle(
+    //                 fontSize: widget.valueStyle == null
+    //                     ? 15
+    //                     : widget.valueStyle!.fontSize))
+    //         .width
+    //         .round());
+  }
 
   @override
   Widget build(BuildContext context) {
-    return selectable == null || !selectable!
-        ? Row(
-            mainAxisSize: MainAxisSize.min,
-            crossAxisAlignment: CrossAxisAlignment.baseline,
-            textBaseline: TextBaseline.alphabetic,
-            children: [
-              SizedBox(
-                width: keyWidth,
-                child: Text(
-                  keyText,
-                  style: keyStyle ??
-                      TextStyle(
-                        fontSize: 15,
-                        fontWeight: FontWeight.bold,
-                        color: Theme.of(context).hintColor,
-                      ),
-                ),
-              ),
-              SizedBox(
-                width: spaceInBetween,
-              ),
-              valueLoadingAnim == null
-                  ? SizedBox(
-                      width: valueWidth,
-                      child: Text(
-                        valueText,
-                        style: valueStyle ??
-                            TextStyle(
-                              fontSize: 15,
-                              fontWeight: FontWeight.normal,
-                              color: Theme.of(context).colorScheme.onSurface,
-                            ),
-                      ),
-                    )
-                  : valueLoadingAnim!,
-            ],
-          )
-        : SelectableText.rich(
+    // _controller.text = widget.text;
+    final originalText = widget.valueText;
+    final fontSize = widget.valueStyle?.fontSize ?? 15.0;
+    // final color = _modified ? Colors.amber.shade900 : null;
+    String displayText = convertToDisplayString(
+        _currentFullText,
+        (widget.valueWidth ?? 60),
+        widget.valueStyle ?? const TextStyle(fontSize: 15));
+    double stringDisplaySize = 1.13 *
+        getStringDisplaySize(
+                displayText, widget.valueStyle ?? const TextStyle(fontSize: 15))
+            .width;
+    String tooltipText = '';
+    if (displayText != _currentFullText) {
+      tooltipText = _currentFullText;
+    }
+    return (widget.selectable)
+        ? SelectableText.rich(
             getContent(context),
             // textAlign: TextAlign.left,
             // textDirection: TextDirection.ltr,
@@ -76,13 +85,109 @@ class xtKeyValueText extends StatelessWidget {
             // textHeightBehavior: null,
             maxLines: 1,
             // semanticsLabel: null,
-          );
+          )
+        : getNonSelectableContent(context, displayText,
+            tooltipText: tooltipText, stringDisplaySize: stringDisplaySize);
+  }
+
+  Widget getNonSelectableContent(BuildContext context, String displayText,
+      {String tooltipText = '', double stringDisplaySize = 0}) {
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      crossAxisAlignment: CrossAxisAlignment.baseline,
+      textBaseline: TextBaseline.alphabetic,
+      children: [
+        SizedBox(
+          width: widget.keyWidth,
+          child: Text(
+            widget.keyText,
+            style: widget.keyStyle ??
+                TextStyle(
+                  fontSize: 15,
+                  fontWeight: FontWeight.bold,
+                  color: Theme.of(context).hintColor,
+                ),
+          ),
+        ),
+        SizedBox(
+          width: widget.spaceInBetween,
+        ),
+        widget.valueLoadingAnim == null
+            ? SizedBox(
+                width: widget.valueWidth,
+                child: Tooltip(
+                  message: tooltipText,
+                  waitDuration: const Duration(milliseconds: 500),
+                  child: widget.clickCopy
+                      ? WgtPopupButton(
+                          width: stringDisplaySize,
+                          height: 20,
+                          direction: 'right',
+                          popupWidth: 90,
+                          popupHeight: 30,
+                          // backgroundColor: Theme.of(context).colorScheme.primary,
+                          //  Colors.green.shade700,
+                          popupChild: Container(
+                            decoration: BoxDecoration(
+                              color: Theme.of(context)
+                                  .scaffoldBackgroundColor
+                                  .withOpacity(0.1),
+                              border: Border.all(
+                                color: Theme.of(context)
+                                    .hintColor
+                                    .withOpacity(0.3),
+                              ),
+                              borderRadius: BorderRadius.circular(5),
+                              boxShadow: [
+                                BoxShadow(
+                                  color: Theme.of(context)
+                                      .hintColor
+                                      .withOpacity(0.1),
+                                  spreadRadius: 0,
+                                  blurRadius: 2,
+                                  offset: const Offset(0, 2),
+                                ),
+                              ],
+                            ),
+                            child: const Align(
+                              alignment: Alignment.center,
+                              child: Text('Copied'),
+                            ),
+                          ),
+                          child: Text(
+                            displayText,
+                            style: widget.valueStyle ??
+                                TextStyle(
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.normal,
+                                  color:
+                                      Theme.of(context).colorScheme.onSurface,
+                                ),
+                          ),
+                          onTap: () {
+                            Clipboard.setData(
+                                ClipboardData(text: _currentFullText));
+                          },
+                        )
+                      : Text(
+                          widget.valueText,
+                          style: widget.valueStyle ??
+                              TextStyle(
+                                fontSize: 15,
+                                fontWeight: FontWeight.normal,
+                                color: Theme.of(context).colorScheme.onSurface,
+                              ),
+                        ),
+                ))
+            : widget.valueLoadingAnim!,
+      ],
+    );
   }
 
   TextSpan getContent(BuildContext context) {
     return TextSpan(
-      text: keyText,
-      style: keyStyle ??
+      text: widget.keyText,
+      style: widget.keyStyle ??
           TextStyle(
             fontSize: 15,
             fontWeight: FontWeight.bold,
@@ -91,13 +196,13 @@ class xtKeyValueText extends StatelessWidget {
       children: [
         WidgetSpan(
           child: SizedBox(
-            width: spaceInBetween,
+            width: widget.spaceInBetween,
           ),
         ),
-        valueLoadingAnim == null
+        widget.valueLoadingAnim == null
             ? TextSpan(
-                text: valueText,
-                style: valueStyle ??
+                text: widget.valueText,
+                style: widget.valueStyle ??
                     TextStyle(
                       fontSize: 15,
                       fontWeight: FontWeight.normal,
@@ -105,7 +210,7 @@ class xtKeyValueText extends StatelessWidget {
                     ),
               )
             : WidgetSpan(
-                child: valueLoadingAnim!,
+                child: widget.valueLoadingAnim!,
                 alignment: PlaceholderAlignment.middle,
               ),
       ],
