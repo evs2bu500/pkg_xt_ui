171271929054c7f89f9269ebcfd08dc7bb1d160e
ggbotbox <oliver.lu@pa.com.sg>
2024-02-25 13:36:34 +0800

    misc

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/pa596/dev/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/pa596/Developer/_evs2/_evs2op/20231208/pkg_xt_ui"
+export "FLUTTER_ROOT=C:\src\flutter"
+export "FLUTTER_APPLICATION_PATH=D:\_pa\_evs2\_svc_evs2op\20240223\pkg_xt_ui"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
-export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
-export "FLUTTER_BUILD_NAME=1.7.12"
-export "FLUTTER_BUILD_NUMBER=1.7.12"
+export "FLUTTER_BUILD_NAME=1.8.3"
+export "FLUTTER_BUILD_NUMBER=1.8.3"
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=true"
 export "TREE_SHAKE_ICONS=false"
